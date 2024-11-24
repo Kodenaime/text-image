d5c62b8c96bb313444bd2e5b0794f81befa85ffb
iamkodenaime <creativedannyk@gmail.com>
2024-11-24 09:56:45 +0100

    working on API for clipdrop

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,17 @@
 import express from 'express'
-import {registerUser, loginUser} from '../controllers/userController.js'
+import {registerUser, loginUser, userCredits} from '../controllers/userController.js'
+import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
+userRouter.post('/credits', userAuth, userCredits)
 
 export default userRouter
 
 
 // port for register http://localhost:4000/api/user/register
 // port for login http://localhost:4000/api/user/login
+
+// port for user credits http://localhost:4000/api/user/credits
