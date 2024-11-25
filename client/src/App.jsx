9b67080afe29e41d2a098975287f149a15dcfb67
iamkodenaime <creativedannyk@gmail.com>
2024-11-25 12:54:32 +0100

    working on local integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 
 import { Routes, Route } from 'react-router-dom'
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from './pages/Home'
 import Result from './pages/Result'
 import Subscribe from './pages/Subscribe'
@@ -16,6 +19,7 @@ const App = () => {
   
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orangered-50'>
+      <ToastContainer position='bottom-right' />
       <Navbar />
       {
         showLogin && <Login />
--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,8 +1,10 @@
-import userModel from "../models/userModel"
+import axios from "axios"
+import userModel from "../models/userModel.js"
+import FormData from 'form-data'
 
 
 export const generateImage = async (req, res) => {
-    try {
+    try { 
         const {userId, prompt} = req.body
 
         const user = await userModel.findById(userId)
@@ -14,6 +16,23 @@ export const generateImage = async (req, res) => {
         if(user.creditBalance === 0 || userModel.creditBalance < 0) {
             return res.json({success: false, message: 'No Credits Available', creditBalance: user.creditBalance})
         }
+
+        const formData = new FormData()
+        formData.append('prompt', prompt) 
+
+        const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {
+            headers: {
+                'x-api-key': process.env.CLIPDROP_API,
+              },
+              responseType: 'arraybuffer'
+        })
+
+        const base64Image = Buffer.from(data, 'binary').toString('base64')
+        const resultImage = `data:image/png;base64,${base64Image}`
+
+        await userModel.findByIdAndUpdate(user._id, {creditBalance: user.creditBalance - 1})
+
+        res.json({success: true, message: 'Image Generated', creditBalance: user.creditBalance - 1, resultImage })
 
         
         
